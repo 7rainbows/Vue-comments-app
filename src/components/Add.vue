beab6e9bdab1caa4663e4e984549e96efaebeecb
1201tengyang <tengyangcc@163.com>
2017-11-01 21:12:46 +0800

    first commit

---
+++ src/components/Add.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="col-md-4">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label>用户名</label>
+        <input type="text" class="form-control" placeholder="用户名" v-model="userName">
+      </div>
+      <div class="form-group">
+        <label>评论内容</label>
+        <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default pull-right" @click.prevent="addItem">提交</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      addComment: {
+        type: Function,
+        isRequire: true
+      }
+    },
+    data () {
+      return {
+        userName: '',
+        content: ''
+      }
+    },
+    methods: {
+      addItem () {
+        let userName = this.userName.trim()
+        let content = this.content.trim()
+        if (!userName || !content) {
+          alert('请输入用户名或内容')
+          return
+        }
+        let comment = {
+          userName,
+          content
+        }
+        this.addComment(comment)
+        this.userName = ''
+        this.content = ''
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue.js的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"/>
+      <List :comments="comments" :removeComment="removeComment"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Add from './Add.vue'
+  import List from './List.vue'
+  export default {
+    data () {
+      return {
+        comments: [
+          {userName: '尤雨溪', content: '不吹不黑,我Vue最牛逼!!'},
+          {userName: '大漠穷秋', content: '垃圾渣渣，抄袭我Angular,菜机！！'}
+        ]
+      }
+    },
+    components: {
+      Add,
+      List
+    },
+    methods: {
+      addComment (comment) {
+        this.comments.unshift(comment)
+      },
+      removeComment (index) {
+        this.comments.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
